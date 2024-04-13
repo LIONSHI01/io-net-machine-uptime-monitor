ed35000eabe2a8d3b090efa78307dac85a3bf9a2
lionc <shizijian2010@yahoo.com.hk>
2024-04-14 06:09:36 +0800

    add logger

---
+++ src/utils/utils.ts
@@ -0,0 +1,17 @@
+import { Logger } from "pino";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const retrieveEnvVariable = (variableName: string, logger: Logger) => {
+  const variable = process.env[variableName] || "";
+  if (!variable) {
+    logger.error(`${variableName} is not set`);
+    process.exit(1);
+  }
+  return variable;
+};
+
+export async function delay(ms: number) {
+  await new Promise((resolve) => setTimeout(resolve, ms));
+}
